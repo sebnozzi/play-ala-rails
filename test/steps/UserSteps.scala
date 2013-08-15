b6d7205be0637ea95f0c81e681110ee2f314523e
sebnozzi <sen@192.168.0.12>
2013-08-15 15:56:38 +0200

    Step implementation using DAO

--- test/steps/UserSteps.scala
+++ test/steps/UserSteps.scala
@@ -3,12 +3,13 @@ package steps
 import cucumber.api.scala.{ ScalaDsl, EN }
 import org.scalatest.matchers.ShouldMatchers
 import cucumber.runtime.PendingException
+import testhelpers.PlayCucumberSupport
+import daos.UserDao
 
-class UserSteps extends ScalaDsl with EN with ShouldMatchers {
+class UserSteps extends ScalaDsl with EN with ShouldMatchers with PlayCucumberSupport {
 
   Given("""^that user "([^"]*)" exists$""") { (username: String) =>
-    //// Express the Regexp above with the code you wish you had
-    throw new PendingException()
+    UserDao.createUser(username)
   }
   
 }
