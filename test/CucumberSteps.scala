51d6b80eba48bdf1c9286b3deec9400fe7274596
sebnozzi <sen@192.168.0.12>
2013-08-15 12:53:51 +0200

    Implementation of the dummy feature

---
+++ test/CucumberSteps.scala
@@ -0,0 +1,21 @@
+import cucumber.api.scala.{ScalaDsl, EN}
+import org.scalatest.matchers.ShouldMatchers
+
+class CucumberSteps extends ScalaDsl with EN with ShouldMatchers {
+
+  private var givenCalled = false
+  private var whenCalled = false
+
+  Given("""^A SBT project$""") {
+    givenCalled = true
+  }
+
+  When("""^I run the cucumber goal$""") {
+    whenCalled = true
+  }
+
+  Then("""^Cucumber is executed against my features and step definitions$""") {
+    givenCalled should be (true)
+    whenCalled should be (true)
+  }
+}
