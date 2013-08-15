c02c9ebe7fb0771ddd952c6eec63aa9ec4bcf758
sebnozzi <sen@192.168.0.12>
2013-08-15 14:51:15 +0200

    Extracted FakeApplication creation with in-memory-db

--- test/daos/UserDaoSpec.scala
+++ test/daos/UserDaoSpec.scala
@@ -1,21 +1,25 @@
 package daos
 
 import org.specs2.mutable._
-
 import play.api.test._
 import play.api.test.Helpers._
-
 import models._
+import org.specs2.execute.Result
+import org.specs2.execute.AsResult
 
 class UserDaoSpec extends Specification {
 
+  def appWithInMemoryDb() = FakeApplication(additionalConfiguration = inMemoryDatabase())
+
   "UserDao" should {
     "create an user" in {
-      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+      running(appWithInMemoryDb) {
         val user: User = UserDao.createUser(username = "manager")
         assert(user.username === "manager")
       }
     }
+
+
   }
 
 }
