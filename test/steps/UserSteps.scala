3021faa06ff776988f92da2221a7827b4614d761
sebnozzi <sen@192.168.0.12>
2013-08-15 23:56:06 +0200

    step to add posts

--- test/steps/UserSteps.scala
+++ test/steps/UserSteps.scala
@@ -4,11 +4,21 @@ import cucumber.api.scala.{ ScalaDsl, EN }
 import org.scalatest.matchers.ShouldMatchers
 import testhelpers.PlayCucumberSupport
 import daos.UserDao
+import cucumber.api.DataTable
+import scala.collection.JavaConversions._
+import daos.PostDao
 
 class UserSteps extends ScalaDsl with EN with ShouldMatchers with PlayCucumberSupport {
 
   Given("""^that user "([^"]*)" exists$""") { (username: String) =>
     UserDao.createUser(username)
+  }
+  
+  Given("""^that user "([^"]*)" wrote the following posts$""") { (username: String, table: DataTable) =>
+    val user = UserDao.findByUsername(username).get
+    table.flatten().foreach{ postText => 
+      PostDao.createPost(user, postText)
+    }
   }
   
 }
