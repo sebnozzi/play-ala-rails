4f6159f78fd9225e44df76b9fefe238b4dc01ed4
sebnozzi <sen@192.168.0.12>
2013-08-15 18:31:34 +0200

    Removed default index page of Play

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,6 @@ import play.api.mvc._
 
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
+  def index = TODO
   
 }
