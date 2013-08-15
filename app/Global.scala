76b28442891d055df6d1d6111243219f553e5f08
sebnozzi <sen@192.168.0.12>
2013-08-15 14:59:57 +0200

    Refactoring

--- app/Global.scala
+++ app/Global.scala
@@ -1,6 +1,6 @@
-import org.squeryl.adapters.{H2Adapter, MySQLAdapter}
+import org.squeryl.adapters.{ H2Adapter, MySQLAdapter }
 import org.squeryl.internals.DatabaseAdapter
-import org.squeryl.{Session, SessionFactory}
+import org.squeryl.{ Session, SessionFactory }
 import play.api.db.DB
 import play.api.GlobalSettings
 
@@ -9,6 +9,10 @@ import play.api.Application
 object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
+    initDbSession(app)
+  }
+
+  def initDbSession(app: Application) {
     SessionFactory.concreteFactory = app.configuration.getString("db.default.driver") match {
       case Some("org.h2.Driver") => Some(() => getSession(new H2Adapter, app))
       case Some("com.mysql.jdbc.Driver") => Some(() => getSession(new MySQLAdapter, app))
@@ -16,6 +20,6 @@ object Global extends GlobalSettings {
     }
   }
 
-  def getSession(adapter:DatabaseAdapter, app: Application) = Session.create(DB.getConnection()(app), adapter)
+  def getSession(adapter: DatabaseAdapter, app: Application) = Session.create(DB.getConnection()(app), adapter)
 
 }
