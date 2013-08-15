8c903cfc3a7d7c63c39388bd077f52daaa7c06ac
sebnozzi <sen@192.168.0.12>
2013-08-15 12:42:40 +0200

    Added Cucumber settings to SBT build

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,7 @@
 import sbt._
 import Keys._
 import play.Project._
+import templemore.sbt.cucumber.CucumberPlugin
 
 object ApplicationBuild extends Build {
 
@@ -16,6 +17,7 @@ object ApplicationBuild extends Build {
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
+    CucumberPlugin.cucumberSettings: _*
   )
 
 }
