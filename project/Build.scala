f84925e38bce19bb2276e796487f0e76bcf8f9b4
sebnozzi <sen@192.168.0.12>
2013-08-18 22:05:28 +0200

    Interactive console creates a StaticApplication instance

--- project/Build.scala
+++ project/Build.scala
@@ -5,18 +5,18 @@ import templemore.sbt.cucumber.CucumberPlugin
 
 object ApplicationBuild extends Build {
 
-  val appName         = "PlayAlaRails"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "PlayAlaRails"
+  val appVersion = "1.0-SNAPSHOT"
 
-  val scalaTest = "org.scalatest" % "scalatest_2.10" % "2.0.M5b" % "test"  
+  val scalaTest = "org.scalatest" % "scalatest_2.10" % "2.0.M5b" % "test"
   val activeRecord = "com.github.aselab" %% "scala-activerecord" % "0.2.2"
-  val mysql = "mysql" % "mysql-connector-java" % "5.1.12"    
+  val mysql = "mysql" % "mysql-connector-java" % "5.1.12"
   val flyway = "com.googlecode.flyway" % "flyway-core" % "2.1.1"
-  
-  val cucumberScala =  "info.cukes" % "cucumber-scala" % "1.1.2" % "test"
-  
-  val phantomJsDriver = "com.github.detro.ghostdriver" % "phantomjsdriver" % "1.0.3" % "test"  
-  
+
+  val cucumberScala = "info.cukes" % "cucumber-scala" % "1.1.2" % "test"
+
+  val phantomJsDriver = "com.github.detro.ghostdriver" % "phantomjsdriver" % "1.0.3" % "test"
+
   val appDependencies = Seq(
     // Add your project dependencies here
     scalaTest,
@@ -25,17 +25,28 @@ object ApplicationBuild extends Build {
     phantomJsDriver,
     flyway,
     cucumberScala,
-    jdbc
-  )
+    jdbc)
 
   val cucumberSettings = CucumberPlugin.cucumberSettings ++
     Seq(
-        CucumberPlugin.cucumberFeaturesLocation := "./test/features",
-        CucumberPlugin.cucumberStepsBasePackage := "steps")
+      CucumberPlugin.cucumberFeaturesLocation := "./test/features",
+      CucumberPlugin.cucumberStepsBasePackage := "steps")
+
+  val consoleInitialStatements = """
+    |import play.core.StaticApplication
+    |val app =  new StaticApplication(new java.io.File("."))
+    |import models._
+    |println("Play application initialized. Models imported.")
+    """.stripMargin
+   
+  val mySettings = Seq(
+      initialCommands in console := consoleInitialStatements
+    ) ++ cucumberSettings
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
-    cucumberSettings: _*
-  )
+    mySettings: _*)
+
+  // val app =  new StaticApplication(new java.io.File("."))
 
 }
