00820891009ae6a0c05c32ff45bf4f558aed528e
sebnozzi <sen@192.168.0.12>
2013-08-19 22:46:31 +0200

    New feature: username links to posts

--- test/features/list-of-users.feature
+++ test/features/list-of-users.feature
@@ -1,10 +1,18 @@
 Feature: List of users
 
   In order to see who uses the system, I want to see a list of users
+
+  Background:
+    Given that user "Homer" exists
+    And that user "Smithers" exists
   
   Scenario: See the list of users
-    Given that user "manager" exists
-    And that user "secretary" exists
     When I go to the "user list" page
-    Then I should see "manager"
-    And I should see "secretary"
+    Then I should see "Homer"
+    And I should see "Smithers"
+    
+  Scenario: Click on a user, land on the posts page
+    When I go to the "user list" page
+    And I click on "Homer"
+    Then I should be on the posts page of "Homer"
+ 
--- test/steps/WebSteps.scala
+++ test/steps/WebSteps.scala
@@ -2,16 +2,12 @@ package steps
 
 import play.api._
 import play.api.mvc._
-
 import cucumber.api.scala.{ ScalaDsl, EN }
-
 import org.scalatest.matchers.ShouldMatchers
 import testhelpers.PlayCucumberSupport
-
 import org.openqa.selenium.By
-
 import models._
-
+import java.util.concurrent.TimeUnit
 
 class WebSteps extends ScalaDsl with EN with ShouldMatchers with PlayCucumberSupport {
 
@@ -34,10 +30,25 @@ class WebSteps extends ScalaDsl with EN with ShouldMatchers with PlayCucumberSup
     val pageUrl = controllers.routes.UserActions.posts(user.id).url
     browser.goTo(baseUrl + pageUrl)
   }
-  
+
   Then("""^I should see (\d+) posts$""") { (expectedAmountOfPosts: Int) =>
     val elements = driver.findElements(new By.ByClassName("post"))
     elements should have length expectedAmountOfPosts
+  }
+
+  When("""^I click on "([^"]*)"$""") { (username: String) =>
+    val startUrl = driver.getCurrentUrl()
+    val element = driver.findElement(new By.ByPartialLinkText(username))
+    browser.waitUntil {
+      element.click()
+      driver.getCurrentUrl() != startUrl
+    }
+  }
+
+  Then("""^I should be on the posts page of "([^"]*)"$""") { (username: String) =>
+    val user = User.findBy("username", username).get
+    val expectedUrl = controllers.routes.UserActions.posts(user.id).url
+    (driver.getCurrentUrl()) should endWith(expectedUrl)
   }
 
 }
